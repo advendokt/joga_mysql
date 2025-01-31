a09fdf31a544af7e7577807c1315a8c15d29f2cd
advendokt <a2vend0kt@gmail.com>
2025-01-31 12:11:35 +0300

    Display all articles

---
+++ routes/articles.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const articleController = require('../controllers/articles');
+
+
+router.get('/', articleController.getAllArticles);
+
+module.exports = router;
